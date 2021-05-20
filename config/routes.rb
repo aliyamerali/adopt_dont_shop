136d8f35050f2e410ade8808278ad22398fbd3b9
Aliya Merali <merali4@gmail.com>
2021-05-20 17:25:39 -0600

    Add route, model method, controller action, view for reverse alpha admin shelter index

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+  #Admin shelter view
+  get '/admin/shelters/', to: 'shelters#admin_index'
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
