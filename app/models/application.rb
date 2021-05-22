53f6ebaec9b8608a4b315a1125d9c948596cee84
Aliya Merali <merali4@gmail.com>
2021-05-22 09:31:19 -0600

    Add destroy all to dependency statements

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,4 @@
 class Application < ApplicationRecord
-  has_many :applications_pets
-  has_many :pets, through: :applications_pets
+  has_many :applications_pets, dependent: :destroy
+  has_many :pets, through: :applications_pets, dependent: :destroy
 end
