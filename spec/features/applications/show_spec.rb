e54c1d9f17f48f76106842a47bbe83606af67c60
Aliya Merali <merali4@gmail.com>
2021-05-22 10:13:51 -0600

    Add test for section to exist based on application status

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 RSpec.describe "Application show page", type: :feature do
   before :each do
     @shelter = Shelter.create!(name: "Denver Animal Rescue", city: "Denver", rank: 1, foster_program: true)
-    @application = Application.create!(name: "Aliya",
+    @application1 = Application.create!(name: "Aliya",
                                       street_address: "1243 N Lafayette",
                                       city: "Denver",
                                       state: "CO",
                                       zip_code: 80218,
                                       description: "I love animals!",
                                       status: "Pending")
+    @application2 = Application.create!(name: "Tanner",
+                                      street_address: "1241 N Lafayette",
+                                      city: "Denver",
+                                      state: "CO",
+                                      zip_code: 80218,
+                                      description: "Dogs are the best!",
+                                      status: "In Progress")
     @alfalfa = Pet.create!(adoptable: true,
                             age: 3,
                             breed: "rescue special",
@@ -21,19 +28,19 @@ RSpec.describe "Application show page", type: :feature do
                             name: "Sprout",
                             shelter_id: @shelter.id)
 
-    ApplicationsPet.create!(pet: @alfalfa, application: @application)
-    ApplicationsPet.create!(pet: @sprout, application: @application)
+    ApplicationsPet.create!(pet: @alfalfa, application: @application1)
+    ApplicationsPet.create!(pet: @sprout, application: @application1)
 
-    visit "/applications/#{@application.id}"
+    visit "/applications/#{@application1.id}"
   end
 
   it 'shows the application attributes' do
-    expect(page).to have_content(@application.name)
-    expect(page).to have_content(@application.street_address)
-    expect(page).to have_content(@application.city)
-    expect(page).to have_content(@application.state)
-    expect(page).to have_content(@application.zip_code)
-    expect(page).to have_content(@application.description)
+    expect(page).to have_content(@application1.name)
+    expect(page).to have_content(@application1.street_address)
+    expect(page).to have_content(@application1.city)
+    expect(page).to have_content(@application1.state)
+    expect(page).to have_content(@application1.zip_code)
+    expect(page).to have_content(@application1.description)
   end
 
   it 'shows the names of all associated pets, linking to pet show pages' do
@@ -44,7 +51,19 @@ RSpec.describe "Application show page", type: :feature do
   end
 
   it 'shows the application status' do
-    expect(page).to have_content(@application.status)
+    expect(page).to have_content(@application1.status)
+  end
+
+  it 'shows the ability to search pets if the application is not submitted' do
+    visit "/applications/#{@application2.id}"
+
+    expect(page).to have_content("Add a Pet to this Application")
+  end
+
+  it 'does not show the ability to search pets if the application has been submitted' do
+    visit "/applications/#{@application1.id}"
+
+    expect(page).to_not have_content("Add a Pet to this Application")
   end
 
 end
