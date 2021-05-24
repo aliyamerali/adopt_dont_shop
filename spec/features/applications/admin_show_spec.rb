7355dbbff8f789f494706a03ac342eac695ddb6b
Aliya Merali <merali4@gmail.com>
2021-05-24 16:40:54 -0600

    Add test for existance of approve button

--- spec/features/applications/admin_show_spec.rb
+++ spec/features/applications/admin_show_spec.rb
@@ -3,22 +3,32 @@ require 'rails_helper'
 RSpec.describe 'Applications admin show page', type: :feature do
   before(:each) do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
     @pirate = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @clawdia = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @lucille = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @lucille = @shelter_1.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
 
-    @pirate.applications.create!(name: "Zahra",
+    @application = @pirate.applications.create!(name: "Zahra",
                                       street_address: "1000 Park Avenue",
                                       city: "Longmont",
                                       state: "CO",
                                       zip_code: 80503,
                                       description: "I've got a big backyard!",
                                       status: "Pending")
+    @application.pets << @clawdia
+    @application.pets << @lucille
 
-    visit '/admin/applications'
+    visit "/admin/applications/#{@application.id}"
   end
 
-  it ''
+  it 'shows a button for every pet that the application is for, there is a button to approve' do
+    within(".approve-for-#{@pirate.name}") do
+      expect(page).to have_content("Approve")
+    end
+    within(".approve-for-#{@clawdia.name}") do
+      expect(page).to have_content("Approve")
+    end
+    within(".approve-for-#{@lucille.name}") do
+      expect(page).to have_content("Approve")
+    end
+  end
 end
