db3b21b789a2d232d9dcc30927e0effa96b16431
Aliya Merali <merali4@gmail.com>
2021-05-24 17:25:53 -0600

    Add column for status to applications_pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_214525) do
+ActiveRecord::Schema.define(version: 2021_05_24_232447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_05_21_214525) do
   create_table "applications_pets", force: :cascade do |t|
     t.bigint "application_id"
     t.bigint "pet_id"
+    t.string "status"
     t.index ["application_id"], name: "index_applications_pets_on_application_id"
     t.index ["pet_id"], name: "index_applications_pets_on_pet_id"
   end
