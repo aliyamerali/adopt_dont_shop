86938c17d5981bba094b0e84e15b58e56609aafc
Aliya Merali <merali4@gmail.com>
2021-05-26 10:06:42 -0600

    Add migration to include state as field in shelter table, update test, controller, and view to show full address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_155837) do
+ActiveRecord::Schema.define(version: 2021_05_26_160501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_155837) do
     t.datetime "updated_at", null: false
     t.string "street_address"
     t.integer "zip_code"
+    t.string "state"
   end
 
   create_table "veterinarians", force: :cascade do |t|
