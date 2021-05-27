56d0ef1cfc1003514b2aab9cd6d1fe40cd1aad5b
Aliya Merali <merali4@gmail.com>
2021-05-26 19:57:18 -0600

    Cleanup test files

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -21,13 +21,13 @@ RSpec.describe Pet, type: :model do
   end
 
   describe 'class methods' do
-    describe '#search' do
+    describe '::search' do
       it 'returns partial matches' do
         expect(Pet.search("Claw")).to eq([@pet_2])
       end
     end
 
-    describe '#search_adoptable' do
+    describe '::search_adoptable' do
       it 'returns adoptable pets with partially matching names' do
         expect(Pet.search_adoptable("Claw")).to eq([@pet_2])
       end
@@ -36,7 +36,7 @@ RSpec.describe Pet, type: :model do
       end
     end
 
-    describe '#adoptable' do
+    describe '::adoptable' do
       it 'returns adoptable pets' do
         expect(Pet.adoptable).to eq([@pet_1, @pet_2])
       end
@@ -44,7 +44,7 @@ RSpec.describe Pet, type: :model do
   end
 
   describe 'instance methods' do
-    describe '.shelter_name' do
+    describe '#shelter_name' do
       it 'returns the shelter name for the given pet' do
         expect(@pet_3.shelter_name).to eq(@shelter_1.name)
       end
