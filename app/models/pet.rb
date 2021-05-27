4ae6db568df216fd79e427621445d20dd38a8bc1
Aliya Merali <merali4@gmail.com>
2021-05-27 10:35:51 -0600

    Add scope to pet model to replace adoptable method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,18 +1,21 @@
 class Pet < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true, numericality: true
+
   belongs_to :shelter
   has_many :applications_pets, dependent: :destroy
   has_many :applications, through: :applications_pets, dependent: :destroy
+
+  scope :adoptable, -> { where(adoptable: true)}
 
 
   def shelter_name
     shelter.name
   end
 
-  def self.adoptable
-    where(adoptable: true)
-  end
+  # def self.adoptable
+  #   where(adoptable: true)
+  # end
 
   def self.search_adoptable(name)
     where("adoptable = ? AND name ILIKE ?", true, "%#{name}%")
