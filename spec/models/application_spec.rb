25c6c3a993611e04e9298680866ea4e1a1bc50e3
Aliya Merali <merali4@gmail.com>
2021-05-27 18:16:10 -0600

    Add model method for submittable to pull logic from view

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -25,5 +25,25 @@ RSpec.describe Application, type: :model do
 
       expect(application.pets.length).to eq(1)
     end
+
+    it '#submittable? returns true if app is in progress there is 1+ pet' do
+      shelter = Shelter.create!(name: "Denver Animal Rescue", city: "Denver", rank: 1, foster_program: true)
+      pet = shelter.pets.create!(name: "Linus", breed: "Pitbull", age: 3, adoptable: true)
+
+      application = Application.create!(name: "Aliya",
+                                        street_address: "2525 Broad Street",
+                                        city: "Denver",
+                                        state: "CO",
+                                        zip_code: 80218,
+                                        description: "I love animals!",
+                                        status: "Pending")
+
+      expect(application.submittable?).to eq(false)
+
+      application.add_pet(pet)
+      application.update(status: "In Progress")
+
+      expect(application.submittable?).to eq(true)
+    end
   end
 end
